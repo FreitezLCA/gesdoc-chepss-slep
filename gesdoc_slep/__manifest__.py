8cf1d449dd08b857056b62db0c03e08443bdcb70
Cesar Freitez <cesar.freitez@chepss.cl>
2022-12-19 23:40:14 +0000

    [ADD] Primer commit del módulo de gestión documental SLEP

---
+++ gesdoc_slep/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Gesdoc Slep',
+    'sumary': """Módulo de Gestión documental SLEP""",
+    'description':"""
+        Gestión documental SLEP Chepss:
+        - Gestión documental
+        - Documentos
+        - Empleados
+    """,
+    'author': 'CHEPSS',
+    'website': 'https://www.chepss.cl',
+    'category': 'Digitalización',
+    'version': '0.1',
+    'depends': ['hr','documents'],
+    'license': 'LGPL-3',
+    'data':[
+    ],
+    'demo':[
+        
+    ],
+    
+}
